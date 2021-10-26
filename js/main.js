302471a8ffcea505fa3cfeacde95de667d806d50
jinju-choi <cjj4643@gmail.com>
2021-10-26 22:13:01 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,269 @@
+
+siteMapOpen();
+searchBoxOpen();
+fixedHeader();
+scrollTopBtn();
+siteSelect();
+sellerImgChange();
+//dotClickSlide();
+
+window.onscroll = function () {
+  let winTop = Math.floor(window.scrollY || document.documentElement.scrollTop);
+  fixedHeader(winTop);
+  scrollTopBtn(winTop);
+  //brandTitleMove(winTop);
+  //console.log(winTop);
+  //wheelMove();
+  wheelScoll(winTop);
+}
+
+setInterval(bubleAni, 2000);
+//setInterval(slide, 500);
+
+//title slide 
+new Swiper('#title .swiper-container', {
+  effect: 'fade',
+  autoplay: true,
+  pagination: {
+    el: '#title .swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '#title .swiper-button-next',
+    prevEl: '#title .swiper-button-prev',
+  },
+});
+
+//nav slide
+new Swiper('.nav_slide.swiper-container', {
+  effect: 'fade',
+  autoplay: true,
+  pagination: {
+    el: '.nav_slide .swiper-pagination',
+    clickable: true,
+  },
+});
+
+//brand srory slide
+new Swiper('#brand_story .swiper-container', {
+  autoplay: true,
+  loop: true,
+  pagination: {
+    el: '#brand_story .swiper-pagination',
+    clickable: true,
+  }
+});
+
+// experience slide 
+new Swiper('#experience .swiper-container', {
+  autoplay: true,
+  loop: true,
+  navigation: {
+    nextEl: '#experience .swiper-button-next',
+    prevEl: '#experience .swiper-button-prev',
+  },
+});
+
+//brand_material 버블 애니메이션
+let bubble = document.getElementsByClassName("bubble");
+let brand_material = document.getElementById("brand_material");
+let bubbleBox = brand_material.getElementsByTagName("a");
+let random;
+let random2;
+
+let bubleAni = function(){
+  let count = bubble.length;
+  if(count >= 12){
+    while (bubbleBox.lastChild) {
+      bubbleBox.removeChild(bubbleBox.lastChild);
+    }
+  } else{
+    for(let i = 0; i < bubbleBox.length; i++){
+      let clone = bubble[0].cloneNode();
+      random = Math.floor(Math.random()*100)+1;
+      random2 = Math.floor(Math.random()*200+Math.random()*10);
+
+      bubbleBox[i].appendChild(clone);
+
+      bubble[i].style.transform = `translateY(${random2}px)`;
+      bubble[i].style.left = `${random}%`;
+
+    }
+  }
+
+}
+
+//스크롤시 header 배경변경
+function fixedHeader(win) {
+  let header = document.getElementsByTagName("header")[0];
+  let header_h = header.offsetHeight;
+
+  if (win >= header_h) {
+    header.classList.add("ch_header");
+  } else {
+    header.classList.remove("ch_header");
+  }
+}
+
+//sitemap 버튼 클릭시 사이트맵 나옴
+function siteMapOpen() {
+  let siteBtn = document.getElementsByClassName("site_map")[0];
+  let siteMap = document.getElementsByTagName("nav")[0];
+  let body = document.getElementsByTagName("body")[0];
+  siteBtn.addEventListener("click", function () {
+    siteBtn.removeAttribute("href");
+
+    if (this.classList.contains("current")) {
+      this.classList.add("remove");
+      siteMap.classList.remove("current");
+      this.classList.remove("current");
+      body.style.overflow = "";
+    } else {
+      this.classList.add("current");
+      siteMap.classList.add("current");
+      this.classList.remove("remove");
+      body.style.overflow = "hidden";
+    }
+
+  });
+}
+
+//검색영역 나옴
+function searchBoxOpen() {
+  let searhBtn = document.getElementsByClassName("search_btn")[0];
+  let searhBox = document.getElementsByClassName("search_box")[0];
+  let closeBtn = searhBox.getElementsByClassName("close")[0];
+  searhBtn.addEventListener("click", function () {
+    this.removeAttribute("href");
+    searhBox.classList.add("current");
+  });
+  closeBtn.addEventListener("click", function () {
+    searhBox.classList.remove("current");
+  });
+}
+
+//top 버튼 클릭시 스크롤, 영역진입 화살표 rotation, 아래로 스크롤
+function scrollTopBtn(winT) {
+  let topBtn = document.getElementsByClassName("scroll_top")[0];
+  let ch_point = document.getElementById("experience").offsetTop;
+  let moveDown = document.getElementById("best_sellers").offsetTop;
+  let header = document.getElementsByTagName("header")[0];
+  let header_h = header.offsetHeight;
+
+  topBtn.addEventListener("click", function () {
+    window.scrollTo({top: 0, behavior: "smooth"});
+  });
+
+  if (winT >= ch_point) {
+    topBtn.classList.add("fixed");
+  } else {
+    topBtn.classList.remove("fixed");
+  }
+
+  if (winT == 0) {
+    topBtn.classList.add("rotate");
+    topBtn.addEventListener("click", function () {
+      scrollTo({top: moveDown - header_h,behavior: "smooth"});
+    });
+  } else {
+    topBtn.classList.remove("rotate");
+  }
+
+}
+
+//사이트 셀랙트 토글
+function siteSelect() {
+  let siteBtn = document.getElementsByClassName("site")[0];
+  siteBtn.addEventListener("click", function () {
+    siteBtn.classList.toggle("current");
+  });
+
+}
+
+function sellerImgChange() {
+  let bestSeller = document.getElementById("best_sellers");
+  let sellerList = bestSeller.getElementsByTagName("li");
+
+  let hover = "hover";
+  let no = "";
+  Array.from(sellerList).forEach(function (element, index) {
+    element.addEventListener("mouseenter", function () {
+      let img = element.getElementsByTagName("img")[0];
+      let getSrc = img.src;
+      let ch_img = getSrc.replace("img", hover);
+
+      //let hov = ch_img.replace("http://localhost:1234", no);
+      //img.src = hov;
+      let ch = document.createElement('img');
+      element.appendChild(ch);
+      element.children[0].setAttribute("src", ch_img);
+    })
+  });
+}
+
+
+let title = document.getElementById("title");
+let nextBtn = title.getElementsByClassName("right")[0];
+let prevBtn = title.getElementsByClassName("left")[0];
+let slideCount = title.getElementsByClassName("slide_count")[0];
+let slideBtn = slideCount.getElementsByTagName("button");
+let title_dot = title.getElementsByClassName("slide_btn")[0].getElementsByTagName("li");
+let dot = title_dot.length;
+let slideList = document.getElementsByClassName("title_slide")[0].children;
+let currentIndex = 0;
+let num = 0;
+nextBtn.addEventListener("click", function () {
+  if (currentIndex == dot) {
+    currentIndex + 1;
+  } else {
+    currentIndex - 1;
+  }
+});
+
+
+
+function reset(content, className) {
+  Array.from(content).forEach(element => {
+    element.classList.remove(className);
+  });
+}
+
+let brandStory = document.getElementById("brand_story");
+let brandTop = Math.floor(brandStory.offsetTop);
+let brandTitle = brandStory.getElementsByTagName('h2')[0];
+let body = document.body;
+let html = document.documentElement;
+let winHeight = Math.max(body.scrollHeight, body.offsetHeight,
+  html.clientHeight, html.scrollHeight, html.offsetHeight);
+let brandstorySlide = 0;
+brandTop = brandTop - winHeight;
+console.log('Height:' + winHeight);
+console.log('brandTop:' + brandTop);
+
+document.addEventListener("wheel", wheelMove, false);
+
+function wheelMove(e) {
+  e.preventDefault();
+  let delta = e.detail *2;
+}
+
+var prevPosition = 0; // 스크롤방향
+
+function wheelScoll(wiT){
+  initPosition = wiT;
+
+  if(wiT >= brandTop && wiT <= brandTop + winHeight){
+    if(initPosition > prevPosition){// 스크롤방향
+       delta = -12;
+       brandstorySlide -= delta;// 스크롤방향n
+       brandTitle.style.right = `${brandstorySlide}px`;
+    }else{
+       delta=+12;
+       brandstorySlide -= delta;// 스크롤방향n
+       brandTitle.style.right = `${brandstorySlide}px`;
+    }
+    prevPosition = initPosition;
+  }
+  console.log(brandstorySlide);
+}
+
